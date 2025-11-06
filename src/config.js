e24372f634b65463a585813296b610b21d084039
Gayatri <it.developer1@girikand.com>
2025-11-06 15:11:10 +0530

    Broadcast Socket URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  apiUrl: 'http://192.168.1.14:5000/api',
+  apiUrl: 'http://192.168.1.19:5001/api',
+  socketUrl: 'http://192.168.1.19:5001/appreciationHub',
 };
 
 export default config;
