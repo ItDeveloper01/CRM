52b28a922ad7709450e616e0048ad40c0559ba45
Gayatri <it.developer1@girikand.com>
2025-11-17 15:54:44 +0530

    Removed role based condition for navigation.

--- src/AdminRoute.jsx
+++ src/AdminRoute.jsx
@@ -3,19 +3,24 @@ import { Roles } from './Constants';
 
 export default function AdminRoute({ auth, children }) {
 
+    // If you still need to check if user is logged in:
+  if (!auth) {
+    return <Navigate to="/login" replace />;
+  }
+  
  console.log('auth Object......', auth);
  console.log('children Object......', children);
 
-  if (auth.role !== Roles.SUPER_ADMIN) {
-    if (auth.role !== Roles.ADMIN) {
-      console.log('auth.role', auth.role);
-      return (
-        <Navigate
-          to='/dashboard'
-          replace
-        />
-      );
-    }
-  }
+ // if (auth.role !== Roles.SUPER_ADMIN) {
+  //  if (auth.role !== Roles.ADMIN) {
+      // console.log('auth.role', auth.role);
+      // return (
+      //   <Navigate
+      //     to='/dashboard'
+      //     replace
+      //   />
+      // );
+    //}
+  //}
   return children;
 }
