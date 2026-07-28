870a72ff7d5d028e5e18d08ed377c94c0bb7edf2
PriyankaGirikand <priyankagirikand@gmail.com>
2026-07-28 17:33:33 +0530

    Fetch Dashboard Itinerary from api and  edit create itinerary logic inside handlesave

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,19 +2,20 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 
+//Scroll bar functionality for overall scrm is adjusted from here 
 export default function Layout({ auth, setAuth ,menu }) {
   return (
-    <div className='flex h-screen bg-gray-100'>
+    <div className='flex h-screen overflow-hidden bg-gray-100'>
       <Sidebar
         auth={auth}
         setAuth={setAuth}
       />
-      <div className='flex-1 flex flex-col'>
+      <div className='flex-1 min-w-0 flex flex-col overflow-hidden'>
         <Navbar
           auth={auth}
           setAuth={setAuth}
         />
-        <main className='p-6 overflow-y-auto'>
+        <main className='flex-1 min-w-0 p-6 overflow-y-auto overflow-x-hidden'>
           <Outlet />
         </main>
       </div>
